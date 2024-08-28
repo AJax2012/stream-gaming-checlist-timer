cc9b957556ed2b80a6ccd4eb5cccea58d0cd02e2
ajax <adga2012@gmail.com>
2024-08-27 21:02:56 -0600

    when editing, make edit button darker

--- src/components/Achievements/EditAchievementButtons.tsx
+++ src/components/Achievements/EditAchievementButtons.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { FaEdit } from 'react-icons/fa';
 import AddAchievementButton from './AddAchievementButton';
 import { Button } from '../ui';
@@ -14,8 +15,8 @@ const EditAchievementButtons = ({ isEditMode, toggleEditMode }: Props) => (
         <div className="text-center border-2 rounded-lg border-dashed w-1/2">
           <AddAchievementButton />
         </div>
-        <div className="text-center border-2 rounded-lg border-dashed w-1/2">
-          <Button variant="ghost" className="w-full" onClick={toggleEditMode}>
+        <div className={cn("text-center border-2 rounded-lg w-1/2", {"border": isEditMode, "border-dashed": !isEditMode})}>
+          <Button variant="ghost" className={cn("w-full", {"bg-gray-300 hover:bg-gray-300": isEditMode})} onClick={toggleEditMode}>
             <FaEdit />
           </Button>
         </div>
